b434f49769a31da3d6eaf528ee77e98b9888b74a
Nico Gradiche <gazzottinico19@gmail.com>
2026-03-27 17:55:08 -0300

    fix mysql render

--- db.js
+++ db.js
@@ -2,9 +2,18 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "MYSQL80",
   password: "Nico2314!",
   database: "hotel"
+});
+
+// 👇 AGREGÁ ESTO
+db.connect((err) => {
+  if (err) {
+    console.log("❌ Error MySQL (normal en Render):", err.message);
+  } else {
+    console.log("✅ MySQL conectado");
+  }
 });
 
 module.exports = db;
