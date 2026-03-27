fad273cb2f260203a850601b2dc39d9c94348159
Nico Gradiche <gazzottinico19@gmail.com>
2026-03-27 18:04:06 -0300

    fix port render

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use("/api/disponibilidad", require("./routes/disponibilidad"));
 app.use("/api/reservas", require("./routes/reservas"));
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
 });
