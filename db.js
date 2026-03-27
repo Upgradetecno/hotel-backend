c08727c1d28d562ed75fa66470015828e8ecea31
Nico Gradiche <gazzottinico19@gmail.com>
2026-03-27 18:52:32 -0300

    fix mysql pool connection

--- db.js
+++ db.js
@@ -1,19 +1,14 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: "mysql.railway.internal",
   user: "root",
   password: "olvSSATssNuvCStKPgmKenhXCjSfJPgP",
   database: "railway",
-  port: 3306
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log("❌ Error MySQL:", err.message);
-  } else {
-    console.log("✅ MySQL conectado");
-  }
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
